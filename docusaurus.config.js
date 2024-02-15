ad763351ef094390e555764c7393aecc6b6cb41f
DanielToyama <jswnlinyu@Outlook.com>
2024-02-16 04:23:53 +0800

    first

---
+++ docusaurus.config.js
@@ -0,0 +1,136 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '户山兔兔の小家',
+  tagline: '可爱就是正确！',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://DanielToyama.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'DanielToyama', // Usually your GitHub org/user name.
+  projectName: 'DanielToyama.github.io', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+
+        },
+        blog: {
+          blogSidebarTitle: '最近有什么呢',
+          blogSidebarCount: 10,
+          showReadingTime: true,
+          
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: '户山兔兔の小家',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {to: '/blog', label: '博客', position: 'left'},
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: '兔兔',
+          },
+          
+          {
+            href: 'https://github.com/DanielToyama',
+            label: 'MyGitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'light',
+        links: [
+          {
+            title: '关于我',
+            items: [
+              {
+                label: '兔兔的自我介绍',
+                to: '/docs/intro',
+              },
+              {
+                label: '兔兔的QQ',
+                to: 'https://qm.qq.com/cgi-bin/qm/qr?k=cUojLoZsBE0HvCHgOjuZOGNiJkZ06-z0&noverify=0&personal_qrcode_source=3',
+              },
+            ],
+          },
+          {
+            title: '友情链接',
+            items: [
+              {
+                label: '兔兔的哔哩哔哩主页',
+                href: 'https://b23.tv/U09RKL6',
+              }
+            ],
+          },
+          {
+            title: '还有什么东西呢（？',
+            items: [
+              {
+                label: '博客',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/DanielToyama',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} 兔兔的个人小家. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
---
+++ src/pages/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from '@docusaurus/Link';
+import Layout from '@theme/Layout';
+
+import './index.css'
+
+export default function () {
+    return (
+
+        <Layout description='户山兔兔のBlog站，兔兔的小站，户山兔兔，兔兔，DanielToyama'>
+            <div id='index-page-container'>
+                <div id='index-page-bg'></div>
+                <div id='index-page-bg-mask'></div>
+                <div id='index-page-main'>
+                    <div id='logo'>
+                        <span>兔兔的小窝</span>
+                    </div>
+
+                    <div id='index-page-description'>
+                        欢迎你的来访！！
+                    </div>
+                    <div id='index-page-button-container'>
+                        <div id='index-page-button'>
+                            <Link
+                                className="button button--secondary button--lg"
+                                to="./blog">
+                                敲门
+                            </Link>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
