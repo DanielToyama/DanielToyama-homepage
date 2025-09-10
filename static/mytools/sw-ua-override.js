eab5054ba7da718cd078b718b265835f0e4e9c6a
DanielToyama <jswnlinyu@Outlook.com>
2025-09-10 23:40:51 +0800

    fix

--- static/mytools/sw-ua-override.js
+++ static/mytools/sw-ua-override.js
@@ -1,4 +1,4 @@
-// 从 localStorage 读取自定义 UA，若无则使用默认值
+
 const CUSTOM_UA = localStorage.getItem('customUA') || 'Mozilla/5.0 (Custom-UA)';
 
 // 拦截所有请求并修改 UA
@@ -8,7 +8,7 @@ self.addEventListener('fetch', (event) => {
 
     const newRequest = new Request(event.request, {
         headers: newHeaders,
-        credentials: 'include' // 保留 Cookie 等登录状态
+        credentials: 'include'
     });
 
     event.respondWith(fetch(newRequest));
