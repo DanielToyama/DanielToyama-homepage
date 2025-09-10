05b33711e47b4ceec029bbc02de49f7624242bdd
DanielToyama <jswnlinyu@Outlook.com>
2025-09-10 23:41:57 +0800

    fix

--- static/mytools/sw-ua-override.js
+++ static/mytools/sw-ua-override.js
@@ -1,7 +1,4 @@
-
 const CUSTOM_UA = localStorage.getItem('customUA') || 'Mozilla/5.0 (Custom-UA)';
-
-// 拦截所有请求并修改 UA
 self.addEventListener('fetch', (event) => {
     const newHeaders = new Headers(event.request.headers);
     newHeaders.set('User-Agent', CUSTOM_UA);
